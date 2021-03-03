5377cdd3de82b74d309d8e3a185d2e3a62f029f9
TolulopeVerissimo <t.e.veris15@gmail.com>
2021-03-02 23:01:44 -0500

    Frontend Setup

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { useDispatch } from "react-redux";
+import { restoreUser } from "./Store/session";
+// import { SmoothProvider } from 'react-smooth-scrolling'
+
+
+
+import CookingPromo from './components/CookingPromo'
+import Nav from './components/Hamburger'
+import Toast from './components/TopPage'
+import ProfileIntro from './components/ProfileIntro'
+import WeeklyTrends from './components/WeeklyTrends'
+import LeaderBoards from './components/LeaderBoards'
+import ContactInfo from './components/ContactInfo'
+import PCaro from './components/Carousel';
+
+
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch()
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+
+  useEffect(() => {
+    (async () => {
+      const user = await dispatch(restoreUser())
+      if (!user.errors) {
+        setAuthenticated(true);
+      }
+      setLoaded(true);
+    })();
+  }, [dispatch]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        {/* <SmoothProvider skew={false}> */}
+        {authenticated && <Nav setAuthenticated={setAuthenticated} />}
+        <CookingPromo />
+        <ProfileIntro />
+        <WeeklyTrends />
+        <LeaderBoards />
+        <PCaro />
+        <ContactInfo />
+
+        <Switch>
+          <Route path='/' exact={true}>
+            {authenticated && <LandingPage />}
+            {!authenticated && (
+              <Splash
+                authenticated={authenticated}
+                setAuthenticated={setAuthenticated}
+              />
+            )}
+          </Route>
+
+          <ProtectedRoute path="/:username" exact={true} authenticated={authenticated}>
+            <Profile />
+          </ProtectedRoute>
+
+          <Route path='/sign-up' exact={true}>
+            <SignUpForm
+              setAuthenticated={setAuthenticated}
+            />
+          </Route>
+
+          <ProtectedRoute path='/users' exact={true} authenticated={authenticated}>
+            <UsersList />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            path='/users/:userId'
+            exact={true}
+            authenticated={authenticated}
+          >
+            <User />
+          </ProtectedRoute>
+
+          {/* <ProtectedRoute path='/' exact={true} authenticated={authenticated}>
+            <h1>My Home Page</h1>
+          </ProtectedRoute> */}
+          <Route path="*">
+            <h1 style={{ color: "white" }}>404</h1>
+          </Route>
+
+        </Switch>
+        {/* </SmoothProvider> */}
+      </BrowserRouter>
+    </>
   );
 }
 
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import configureStore from './Store'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ModalProvider } from './Context/Modal';
 
+function Root() {
+  return (
+    <Provider store={store}>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
+    </Provider>
+  );
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
