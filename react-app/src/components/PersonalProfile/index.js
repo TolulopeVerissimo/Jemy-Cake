f89f477de7abb9fbf14bc6a0ffac0e0148cca556
TolulopeVerissimo <t.e.veris15@gmail.com>
2021-03-04 15:38:58 -0500

    cssc
    css

--- react-app/src/components/PersonalProfile/index.js
+++ react-app/src/components/PersonalProfile/index.js
@@ -52,45 +52,59 @@ function PersonalProfile() {
     // 		}
     // 	}
 
-
+    debugger;
     return (
+
         <>
+
             {/* <div className="bodyDiv" style={{ backgroundImage: `url(${backDrop})` }}> */}
             <div className="bodyDiv" >
+
+                <h1 style={{ color: 'white' }}>{user.biography}</h1>
+
 
                 {/* 
                 <div className="RecipeBox">
                     <Combo />
                 </div> */}
 
-                <div className="moveTheFollowButton">
+                {/* <div className="moveTheFollowButton">
                     <FollowUser followedUserId={id} />
-                </div>
+                </div> */}
 
 
                 {/* <div className="cirContainer" >
-                    <div className="centerCircle" style={{ backgroundImage: `url(${image})`, backgroundSize: "cover", backgroundPosition: "center center" }}>
-                        <div className="spinner One" style={{ backgroundImage: `url('https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Pok%C3%A9mon_Pikachu_art.png/220px-Pok%C3%A9mon_Pikachu_art.png')`, backgroundSize: "cover", backgroundPosition: "center center" }}><button onClick={() => <Redirect to="/" />} /></div>
+                    {/* <div className="centerCircle" style={{ backgroundImage: `url(${image})`, backgroundSize: "cover", backgroundPosition: "center center" }}> */}
+                {/* <div className="centerCircle" style={{ backgroundImage: `url('https://www.anime-planet.com/images/characters/ash-ketchum-2804.jpg?t=1541015853)`, backgroundSize: "cover", backgroundPosition: "center center" }}> */}
+                {/* <div className="spinner One" style={{ backgroundImage: `url('https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Pok%C3%A9mon_Pikachu_art.png/220px-Pok%C3%A9mon_Pikachu_art.png')`, backgroundSize: "cover", backgroundPosition: "center center" }}><button onClick={() => <Redirect to="/" />} /></div>
                         <div className="spinner Two" style={{ backgroundImage: `url('https://assets.pokemon.com//assets/cms2/img/video-games/_tiles/pokemon-sword-shield/distributions/pikachu/inline/world.png')`, backgroundSize: "cover", backgroundPosition: "center center" }}><button onClick={() => <Redirect to="/" />} /></div>
                         <div className="spinner Three" style={{ backgroundImage: `url('https://cdn.bulbagarden.net/upload/thumb/4/49/Ash_Pikachu.png/1200px-Ash_Pikachu.png')`, backgroundSize: "cover", backgroundPosition: "center center" }}><button onClick={() => <Redirect to="/" />} /></div>
                         <div className="spinner Four" style={{ backgroundImage: `url('https://media.gamestop.com/i/gamestop/11100921/Pokemon-Pikachu-Plush-24-in?$pdp$')`, backgroundSize: "cover", backgroundPosition: "center center" }}><button onClick={() => <Redirect to="/" />} /></div>
-                        <div className="spinner Five" style={{ backgroundImage: `url('https://www.whats-on-netflix.com/wp-content/uploads/2019/05/Pokemon-Detective-Pikachu-Netflix.jpg')`, backgroundSize: "cover", backgroundPosition: "center center" }}><button onClick={() => <Redirect to="/" />} /></div>
-                    </div>
-                </div> */}
+                        <div className="spinner Five" style={{ backgroundImage: `url('https://www.whats-on-netflix.com/wp-content/uploads/2019/05/Pokemon-Detective-Pikachu-Netflix.jpg')`, backgroundSize: "cover", backgroundPosition: "center center" }}><button onClick={() => <Redirect to="/" />} /></div> */}
+                {/* </div> */}
+                {/* </div>  */}
+
                 <div className="cirContainer" >
+
                     <div className="centerCircle" style={{ backgroundImage: `url(${user.profilePicture})`, backgroundSize: "cover", backgroundPosition: "center center" }}>
+
                         {
-                            users && users.map((el, idx) => (
-                                // <div className=`spinner ${idx}` > <img id="pictureCircle" src={el.ProfilePicture}>
-                                <div className={`spinner ${capitalize(numWords(idx + 1))}`} >
-                                    <img id="pictureCircle" src={el.ProfilePicture} />
-                                </div>
-                            ))
+                            users && users.map((el, idx) => {
+                                debugger;
+                                return (
+                                    // <div className=`spinner ${idx}` > <img id="pictureCircle" src={el.ProfilePicture}>
+                                    <div className={`spinner ${capitalize(numWords(idx + 1))}`} >
+                                        <img id="pictureCircle" src={el.ProfilePicture} />
+                                    </div>
+                                )
+                            }
+                            )
                         }
+
                     </div>
                 </div>
 
-
+                {/* 
                 <div className="PhotoGridsContainer">
                     <div className="PhotoGrid">
                         <Swiper className="gradientSpace" spaceBetween={10} slidesPerView={3} >
@@ -113,8 +127,6 @@ function PersonalProfile() {
                             </article>
 
                             <article class="card">
-                                --image for reflection
-                         {/* <figure style={{--image: "url(https://picsum.photos/id/106/500/250)"}}> */}
                                 <figure style={{ cssProperties }}>
                                     <SwiperSlide>
                                         <img src="https://picsum.photos/id/106/500/250" alt="Flowers under a blue sky." />
@@ -140,16 +152,14 @@ function PersonalProfile() {
                             </article>
 
                             <article class="card">
-                                --image for reflection
-                         {/* <figure style={{--image: "url(https://picsum.photos/id/106/500/250)"}}> */}
                                 <figure style={{ cssProperties }}>
                                     <SwiperSlide>
                                         <img src="https://picsum.photos/id/106/500/250" alt="Flowers under a blue sky." />
                                     </SwiperSlide>
                                 </figure>
-                            </article>
+                            </article> */}
 
-                            {/* {currentRecipeAlbum && currentRecipeAlbum.map(el=>(
+                {/* {currentRecipeAlbum && currentRecipeAlbum.map(el=>(
                                 <article className="card">
                                     <figure style={{ cssProperties }}>
                                     <SwiperSlide>
@@ -157,10 +167,12 @@ function PersonalProfile() {
                                     </SwiperSlide>
                                  </figure>
                                  </article>
-                        ))} */}
+                        ))}
                         </Swiper >
+                         */}
+                {/* </Swiper >
                     </div>
-                </div>
+                </div> */}
             </div>
         </>
     )
